e5ef9b03b6a55ac737cecbdfc2d2aafa33bf19a3
muhammad-muslim <124064868+muhammad-muslim@users.noreply.github.com>
2024-03-02 15:35:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+
+   
+ 
+    var images = [
+      "image/background4.jpg",
+      "image/background2.jpg",
+      "image/background3.jpg",
+    ];
+    var currentIndex = 0;
+    var bgImage = document.querySelector(".bg-image");
+
+    function changeBackgroundImage() {
+      currentIndex = (currentIndex + 1) % images.length;
+      bgImage.style.opacity = 0;
+      setTimeout(function () {
+        bgImage.style.backgroundImage = 'url("' + images[currentIndex] + '")';
+        bgImage.style.opacity = 1;
+      }, 500); // Change opacity after 500ms transition
+    }
+
+    setInterval(changeBackgroundImage, 5000); // Change every 5 seconds
+
+
+    window.addEventListener("scroll", function () {
+      const parallax = document.querySelector(".parallax");
+      let scrollPosition = window.pageYOffset;
+      parallax.style.transform = "translateY(" + scrollPosition * 0.5 + "px)";
+    });
